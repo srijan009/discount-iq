0db461e68c55233e3be95a3b64724d37dcccf2b0
srijan009 <srijan.lama@cartmade.com>
2024-10-02 17:56:23 +0545

    working on cart update and edit storefront api

---
+++ app/routes/api.cart.create/route.tsx
@@ -0,0 +1,11 @@
+import type {
+  ActionFunctionArgs,
+} from "@remix-run/node";
+import { json } from "@remix-run/node";
+export async function action({
+  request,
+}: ActionFunctionArgs) {
+  const cart = await request.json();
+  console.log("cart create",cart)
+  return json({ ok: true, cart });
+}
