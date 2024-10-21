8b210bb508f5b5820ab53a3c0dbb40f54ca69652
srijan009 <srijan.lama@cartmade.com>
2024-10-21 18:10:51 +0545

    working on fetching discount nodes

--- app/routes/api.discount.all/route.tsx
+++ app/routes/api.discount.all/route.tsx
@@ -1,53 +1,37 @@
 import type {
-  ActionFunctionArgs,
-  HeadersFunction,
   LoaderFunctionArgs,
 } from "@remix-run/node";
-import { json } from "@remix-run/node";
 import { authenticate } from "app/shopify.server";
-import { boundary } from "@shopify/shopify-app-remix/server";
-
-export async function action({
-  request,
-}: ActionFunctionArgs) {
-  try{
-    const {admin} = await  authenticate.public.appProxy(request);
-    console.log(request)
-    return json({ status: "success" });
-  }catch(error){
-    console.log(error)
-    return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
-  }
-  // console.log("admin",admin)
-  // const response = await admin.graphql(`
-  // query discountAll{
-  //     discountNodes(first:250,query:"discount_type:app AND status:active"){
-  //       edges{
-  //         node{
-  //           discount{
-  //             __typename
-  //             ... on DiscountAutomaticApp{
-  //               discountId
-  //               __typename
-  //               appDiscountType{
-  //                 appKey
-  //                 functionId
-  //                 discountClass
-  //               }
-  //               status
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
-  // const responseJson = await response.json();
-  // console.log("responseJson", responseJson)
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  const {admin} =  await authenticate.public.appProxy(request);
+  const response = await admin?.graphql(`
+  query discountAll{
+      discountNodes(first:250,query:"discount_type:app AND status:active"){
+        edges{
+          node{
+            discount{
+              __typename
+              ... on DiscountAutomaticApp{
+                discountId
+                __typename
+                appDiscountType{
+                  appKey
+                  functionId
+                  discountClass
+                }
+                status
+              }
+            }
+          metafield(namespace: "$app:gwp-promotions", key: "promoDetails") {
+            value
+          }
+          }
+        }
+      }
+    }
+  `)
+  const result = await response?.json()
+  return { status: "success", data: result?.data}
 }
-// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 
 
-export const headers: HeadersFunction = (headersArgs) => {
-  return boundary.headers(headersArgs);
-};
